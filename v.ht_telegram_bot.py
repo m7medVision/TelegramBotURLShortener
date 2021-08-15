b94836c0db31a4a9337f45f3841dee830ce0a2e1
Mohammed~ <88824957+majhcc@users.noreply.github.com>
2021-08-15 13:48:50 +0400

    Update v.ht_telegram_bot.py

--- v.ht_telegram_bot.py
+++ v.ht_telegram_bot.py
@@ -2,20 +2,20 @@ from pyrogram import Client,filters
 from pyrogram import *
 import requests
 app = Client("my_bot")
-@app.on_message(filters.command("vht"))
+@app.on_message(filters.command(commands="vht",prefixes="."))
 def start_command(client, message):
-    app.send_message(chat_id=message.chat.id, text="يرجى الانتضار..") 
+    app.send_message(chat_id=message.chat.id, text="plase wait") 
     ask1 = message.text
-    ask = ask1.replace("/vht ", "")
+    ask = ask1.replace(".vht ", "")
     ask2 = ask.split()
-    url = ask2[0]
-    name = ask2[1]
+    url = str(ask2[0])
+    name = str(ask2[1])
     m = requests.get("https://v.ht/").headers
     r = m['Set-Cookie']
     removeal = r.replace("; path=/", "")
     data = {
-        'txt_url': ask,
-        'txt_name': ask
+        'txt_url': url,
+        'txt_name': name
     }
     m = {
         'Accept': '*/*',
@@ -37,7 +37,14 @@ def start_command(client, message):
     }
     r = requests.post('https://v.ht/processreq.php', data = data , headers=m).text
     if "Sorry, the entered suffix is already in use, please choose another one..!!" in r:
-        app.send_message(chat_id=message.chat.id, text="هناك خطاء اما من الرابط او البرمجة")
-    else:
+        app.send_message(chat_id=message.chat.id, text="suffix is already in use")
+    elif "Sorry, the entered suffix contains invalid characters," in r:
+        app.send_message(chat_id=message.chat.id, text="suffix contains invalid characters")
+    elif 'The Short URL: ' in r:
         app.send_message(chat_id=message.chat.id, text=(f"https://v.ht/{name}"))
+    else:
+        app.send_message(chat_id=message.chat.id, text="There are mistake in my code :(")
+@app.on_message(filters.command("start"))
+def start_command(client, message):
+    app.send_message(chat_id=message.chat.id, text="Type .vht URL suffix \n\n .vht https://example.com/ example \n\n The shorten URL will be v.ht/example \n\n This bot coded by MAJHCC \n Bot files: https://github.com/majhcc/TelegramBotURLShortener")
 app.run()
